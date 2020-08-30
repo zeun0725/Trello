ab9637c1c56b17b404182ffce401688a76466cd8
parkjieun <wldmsdl3187@naver.com>
2020-08-30 16:54:26 +0900

    login page modify and Authorization setting

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import router from '../router'
+
 const DOMAIN = 'http://localhost:3000'
 const UNAUTHORIZED = 401
 const onUnauthorized = () => {
-  router.push('/login')
+  router.push(`/login?rPath=${encodeURIComponent(location.pathname)}`)
 }
 
 const request = (method, url, data) => {
@@ -14,13 +15,24 @@ const request = (method, url, data) => {
   }).then(result => result.data)
   .catch(result => {
     const {status} = result.response
-    if (status == UNAUTHORIZED) return onUnauthorized()
-    throw Error(result)
+    if (status == UNAUTHORIZED) onUnauthorized()
+    throw result.response
   })
 }
+export const setAuthInHeader = token => {
+  axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
+}
+const {token} = localStorage
+if (token) setAuthInHeader(token)
 
 export const board = {
   fetch(){
     return request('get', '/boards')
+  }
+}
+
+export const auth = {
+  login(email, password){
+    return request('post', '/login', {email, password})
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,17 @@ import NotFound from '../components/NotFound.vue'
 import Card from '../components/Card.vue'
 Vue.use(VueRouter)
 
+const requireAuth = (to, from, next) => { //인증 부분
+  const isAuth = localStorage.getItem('token')
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+  isAuth ? next() : next(loginPath)
+}
+
 export default new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: Home },
-    { path: '/b/:bid', component: Board, children: [
+    { path: '/', component: Home, beforeEnter: requireAuth },
+    { path: '/b/:bid', component: Board, beforeEnter: requireAuth, children: [
       { path:'c/:cid', component: Card }
     ] },
     { path: '/login', component:Login },
